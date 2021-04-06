309e3f8e7ad596822ce0989af72210932a6f7145
remi <rnivet@gmail.com>
2021-04-06 16:18:11 +0200

    Replace anniversaire by naissance

--- fix_member_birthdate.py
+++ fix_member_birthdate.py
@@ -40,28 +40,28 @@ openerp, uid, tz = init_openerp(
 def main():
     # Configure arguments parser
     parser = argparse.ArgumentParser(
-            description='Force la date d\'anniversaire du membre au bon format')
+            description='Force la date de naissance du membre au bon format')
     parser.add_argument('nom', help='Nom du membre (NOM, prenom)')
-    parser.add_argument('anniversaire',
-            help='Date d\'anniversaire (YYYY-MM-DD)')
+    parser.add_argument('date_naissance',
+            help='Date de naissance (YYYY-MM-DD)')
     args = parser.parse_args()
 
     # Check arg format
     try:
-        datetime.strptime(args.anniversaire, '%Y-%m-%d')
+        datetime.strptime(args.date_naissance, '%Y-%m-%d')
     except Exception as e:
         raise Exception('%s : Mauvais format de date (AAAA-MM-JJ)' %\
-                (args.anniversaire))
+                (args.date_naissance))
 
     # Get member from Odoo
     member = openerp.ResPartner.browse([("name", "=", args.nom)])
     if len(member) < 1:
         raise Exception('%s : Membre introuvable dans Odoo' % (args.nom))
 
-    print('Date anniversaire avant : %s' % (member[0].birthdate))
+    print('Date de naissance avant : %s' % (member[0].birthdate))
     print('Modification...')
-    member[0].birthdate = args.anniversaire
-    print('Date aaniversaire après : %s' % (member[0].birthdate))
+    member[0].birthdate = args.date_naissance
+    print('Date de naissance après : %s' % (member[0].birthdate))
 
 if __name__ == "__main__":
     main()
