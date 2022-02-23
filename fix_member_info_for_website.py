e9ac46cbfa00c8575c3c6f5a11586ec91ecda471
remi <rnivet@gmail.com>
2022-02-23 16:49:38 +0100

    Improvements

--- fix_member_info_for_website.py
+++ fix_member_info_for_website.py
@@ -44,17 +44,29 @@ def main():
             description='Force le bypass du paiement dans le shop')
     args = parser.parse_args()
 
+    france = openerp.ResCountry.get(76)
+    count = 0
+
     for member in openerp.ResPartner.browse([("is_worker_member", "=", True)]):
+        changed = False
         if member.skip_website_checkout_payment is False:
             member.skip_website_checkout_payment = True
+            changed = True
         if member.country_id is False:
-            member.country_id = openerp.ResCountry.get(76)
-        if member.phone is False and member.mobile is not False:
+            member.country_id = france
+            changed = True
+        if (member.phone is False or member.phone == "") and member.mobile is not False:
             member.phone = member.mobile
-        print("%s, skip_payment=%s, country=%s, phone=%s" % (member.name,
-            member.skip_website_checkout_payment,
-            member.country_id,
-            member.phone))
+            changed = True
+
+        if changed is True:
+            count+=1
+            print("%s, skip_payment=%s, country=%s, phone=%s" % (member.name,
+                member.skip_website_checkout_payment,
+                member.country_id,
+                member.phone))
+
+    print("Nb membres corrigés :", count)
 
 if __name__ == "__main__":
     main()
